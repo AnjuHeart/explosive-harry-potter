2b57087e31e1cb6ec24f2db816e0e2aeeb34d461
Yhunn <jybrant.efren@gmail.com>
2022-11-25 23:00:00 -0700

    estados y eventos

--- lib/caracteristicas/bloc.dart
+++ lib/caracteristicas/bloc.dart
@@ -1,18 +1,66 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bloc/bloc.dart';
+import 'package:fpdart/fpdart.dart';
 
-class Estado{}
+import 'package:explosive_harry_potter/caracteristicas/verificacion/repositorio_json.dart';
+import 'package:explosive_harry_potter/dominio/problemas.dart';
+import 'package:explosive_harry_potter/dominio/registro_personajes.dart';
 
-class Creandose extends Estado{}
+class Estado {}
 
-class VerPaginaPrincipal extends Estado{}
+class Creandose extends Estado {}
 
-class Evento{}
+class VerPaginaPrincipal extends Estado {}
 
-class Creado extends Evento{}
+class VerListaPeronajes extends Estado {
+  final String tipoPeronaje;
+  final Set<Personaje> personajes;
+  VerListaPeronajes({
+    required this.tipoPeronaje,
+    required this.personajes,
+  });
+}
 
-class BlocPotter extends Bloc<Evento, Estado>{
-  BlocPotter() : super(Creandose()){
-    on<Creado>((event, emit) {
+class VerHechizos extends Estado {}
+
+class VerVaritas extends Estado {}
+
+class ErrorAlCargarJson extends Estado {}
+
+class Evento {}
+
+class Creado extends Evento {}
+
+class SolicitarTodosPersonajes extends Evento {}
+
+class SolicitarPersonajesPorCasa extends Evento {
+  final String casa;
+
+  SolicitarPersonajesPorCasa(this.casa);
+}
+
+class SolicitarAlumnos extends Evento {}
+
+class SolicitarStaff extends Evento {}
+
+class SolicitarHechizos extends Evento {}
+
+class SolicitarVaritas extends Evento {}
+
+class BlocPotter extends Bloc<Evento, Estado> {
+  BlocPotter() : super(Creandose()) {
+    RepositorioJson repositorioJson = RepositorioJsonReal();
+    late Either<Problema, dynamic> respuestaPersonajes;
+    late Either<Problema, dynamic> respuestaHechizos;
+    late dynamic jsonPersonajes;
+    late dynamic jsonHechizos;
+    on<Creado>((event, emit) async {
+      respuestaPersonajes = await repositorioJson.obtenerJsonPersonajes();
+      respuestaHechizos = await repositorioJson.obtenerJsonHechizos();
+      jsonPersonajes =
+          respuestaPersonajes.getOrElse((l) => emit(ErrorAlCargarJson()));
+      jsonHechizos =
+          respuestaHechizos.getOrElse((l) => emit(ErrorAlCargarJson()));
       emit(VerPaginaPrincipal());
     });
   }
