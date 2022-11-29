40835b694d35830c47e97823ce2382b9b2c641d5
Yhunn <jybrant.efren@gmail.com>
2022-11-28 20:58:43 -0700

    repositorio json de pruebas

--- test/caracteristicas/verificacion/repositorio_json_test.dart
+++ test/caracteristicas/verificacion/repositorio_json_test.dart
@@ -35,4 +35,31 @@ void main() {
       expect(y.length, equals(77));
     });
   });
+
+  group("Pruebas offline", () {
+    test("Puedo acceder al primer hechizo", () async {
+      RepositorioJson repo = RepositorioJsonPruebas();
+      var x = await repo.obtenerJsonHechizos();
+      var y = x.getOrElse((l) => null);
+      expect(y[0]["name"] == "Aberto", true);
+    });
+    test("Hay un total de 8 hechizos", () async {
+      RepositorioJson repo = RepositorioJsonPruebas();
+      var x = await repo.obtenerJsonHechizos();
+      var y = x.getOrElse((l) => null);
+      expect(y.length, equals(8));
+    });
+    test("Puedo acceder al primer valor", () async {
+      RepositorioJson repo = RepositorioJsonPruebas();
+      var x = await repo.obtenerJsonPersonajes();
+      var y = x.getOrElse((l) => null);
+      expect(y[0]["name"] == "Harry Potter", true);
+    });
+    test("Hay un total de 11 personajes", () async {
+      RepositorioJson repo = RepositorioJsonPruebas();
+      var x = await repo.obtenerJsonPersonajes();
+      var y = x.getOrElse((l) => null);
+      expect(y.length, equals(11));
+    });
+  });
 }
